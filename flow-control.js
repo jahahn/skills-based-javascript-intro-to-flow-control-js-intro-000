f531fa0296e312bb3446585b7e0dd5f77e320b47
Andrew Hahn <j.andrew.hahn@gmail.com>
2019-05-09 14:44:47 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -10,7 +10,7 @@ function basicTeenager(age) {
 function teenager(age) {
   if (age>12 && age<20)
   {return "You are a teenager!"
-} else (age) {
+} else (undefined) {
     return "You are not a teenager"
   }
 
